c7cb856cb27ee68ce0b89659171966b82bcf1853
zhanglong <315419138@qq.com>
2022-06-20 16:07:48 +0800

    first commit

---
+++ src/router/index.ts
@@ -0,0 +1,28 @@
+import { createRouter, createWebHashHistory, RouteRecordRaw } from "vue-router";
+import Home from "@/views/home.vue";
+// import Pinia from "@/views/pinia.vue";
+
+const routes: Array<RouteRecordRaw> = [
+  {
+    path: "/",
+    name: "Home",
+    component: Home
+  }
+  // {
+  //   path: "/pinia",
+  //   name: "pinia",
+  //   component: Pinia
+  // },
+  // {
+  //   path: "/axios",
+  //   name: "Axios",
+  //   component: () => import("@/views/axios.vue") // 懒加载组件
+  // }
+];
+
+const router = createRouter({
+  history: createWebHashHistory(),
+  routes
+});
+
+export default router;
